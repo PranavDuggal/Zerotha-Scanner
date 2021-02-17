981616d5cca8ecb8b43b8c95b04aa1049cd18a6b
PranavDuggal <70065423+PranavDuggal@users.noreply.github.com>
2021-02-17 16:25:44 +0530

    added template
    added html template in the file

--- Zerotha_scanner/flask_app.py
+++ Zerotha_scanner/flask_app.py
@@ -1,25 +1,36 @@
-# A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, redirect, url_for, request
+
+from flask import Flask, redirect, url_for, request, render_template
 from zerotha_scanner import main
-
+import time
+import sys
+import os, signal
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
+def open_tab():
+   return render_template('flask.html')
 
 @app.route('/',methods = ['POST', 'GET'])
 def call_zerotha():
-   if request.method == 'POST':
-    #   user = request.form['nm']
-      return main()
-#    else:
-#       user = request.args.get('nm')
-#       return redirect(url_for('success',name = user))
-
+   if request.method == "POST":
+      if request.form.get("start"):
+         print('Started')
+         main()
+         return 'Execution Finished'
+      elif request.form.get("stop"):
+         pid = os.getpid()
+         print('Stopped')
+         os.kill(pid,signal.SIGTERM)
+   # elif request.method == 'GET':
+   #      return render_template('flask.html')
+# @app.route('/<Stop>',methods = ['POST', 'GET'])
+# def stop_zerotha(Stop):
+#    print('Hello Pranav')
+#    return sys.exit()
+   # if request.method == 'POST':
+   #    if 'Stop' in request.form:
+   #       sys.exit()
 
 if __name__ == '__main__':
     app.run()
-    
